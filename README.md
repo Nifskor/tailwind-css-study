d3a6f8990d602d0ba04004330a931ce04780fa64
ING'S <nifskr@naver.com>
2024-02-18 03:23:01 +0900

    lesson 10 뱃지 만들기

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # tailwind-css-study
 tailwind를 공부하고 기록한 레포입니다
 
-Lesson 9 카드 만들기 
+Lesson 10 뱃지
+
+1. class 중 relative 속성과 
+2. 내가 넣고자하는 내용을 absolute top-0 클래스를 넣게되면 왼쪽에 뱃지처럼 만들수잇음 
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      colors:{
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
       fontFamily: {
         body: ['Noto Serif KR','serif']
       }
